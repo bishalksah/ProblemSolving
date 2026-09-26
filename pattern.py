5ecf8b1bb3e2f544c256c75599b50ae42c228588
vshalsah29 <vshalsah29@gmail.com>
2026-09-26 20:53:41 +0530

    2nd pattern

---
+++ pattern.py
@@ -0,0 +1,23 @@
+# 5 
+# 4 5 
+# 3 4 5 
+# 2 3 4 5 
+# 1 2 3 4 5 
+
+
+
+# x = 1
+# for i in range(5 ,0 ,-1):
+#     for j in range (i, x+i ,1):
+#         print(j , end =" ")
+        
+#     print()
+#     x += 1
+
+
+
+
+for i in range(5):
+    for j in range (5-i , 6):
+        print(j , end =" ")        
+    print()
