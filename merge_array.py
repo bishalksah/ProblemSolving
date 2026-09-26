b17c7d75bb686ccb94864993abcc5d21e3a6c58d
vshalsah29 <vshalsah29@gmail.com>
2026-09-26 20:39:49 +0530

    1st pattern

--- merge_array.py
+++ merge_array.py
@@ -4,9 +4,26 @@ arr1 = list(map(int, input().split()))
 print("Enter the second array elements separated by space:")
 arr2 = list(map(int, input().split()))
 
-def merge_sort(arr1 ,arr2):
-    merged = arr1 + arr2
-    merged.sort()
-    return merged
+def merge_arrays(arr1 ,arr2):
+    mergedArray = arr1 + arr2
+    sort_array(mergedArray)
+    return mergedArray
 
-print("Merged and sorted array:", merge_sort(arr1, arr2))
+# arr=[6,7,8,9,4,5,6,89,9]
+def sort_array(mergedArray):
+    print(f"{"i":<2} {"j":<2} {"m[i]":<5} {"m[j]":<5}  {"m"}")
+    for i in range(len(mergedArray)):
+        for j in range(i + 1, len(mergedArray)):
+            if mergedArray[i] > mergedArray[j]:
+                mergedArray[i], mergedArray[j] = mergedArray[j], mergedArray[i]
+                
+            print(f"{i:<2} {j:<2} {mergedArray[i]:<5} {mergedArray[j]:<5}  {mergedArray}")
+    return mergedArray
+
+result = merge_arrays(arr1, arr2)
+print("Merged and sorted array:", result)
+    
+  
+    
+    
+    
